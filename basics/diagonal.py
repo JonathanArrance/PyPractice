bf57b6c0f70b87854ee4b3f8eb3cbba946b4f8b0
Jonathan Arrance <jonathan.arrance@gmail.com>
2023-10-23 15:32:05 -0400

    checkpoint

--- basics/diagonal.py
+++ basics/diagonal.py
@@ -11,17 +11,24 @@
 # 9  10 11 12
 # 13 14 15 16
 
+#we will need to play a numbers game and count the number of indexs on each "line" of the matrix.
+#Since a matrix in this case is even width and height and only 2 dimension we can do this with the square root.
+
+
 import math
 
 array = [1, 2, 3, 4, 5, 6 , 7, 8, 9]
 s = int(math.sqrt(len(array)))
 ld = 0
 rd = 0
+#
 for ind in range(0,len(array),s+1):
+    print(f'Looking at the index{ind}: right diag')
     ld += array[ind]
 print(ld)
 
 for ind in range(s-1,len(array),s-1):
+    print(f'Looking at the index{ind}: left diag')
     rd += array[ind]
 rd = rd - array[-1]
 print(rd)
